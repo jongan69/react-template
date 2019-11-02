447c2122b9e3bd9f07a7d1fe157d90eca07e793a
Jonathan Gan <jonny2298@live.com>
2019-11-02 17:44:39 -0400

    Test

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,30 +1,34 @@
 import React, { Component } from 'react';
+import AdSense from 'react-adsense';
 
 import '../App.css';
 
 class Homepage extends Component {
-    render(){
-        return(
-            <header className="App-header">
-              <script data-ad-client="ca-pub-6202902142885850" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+  render() {
+    return (
+      <header className="App-header">
+        <AdSense.Google
+          client='ca-pub-6202902142885850'
+          slot='3693217820'
+        />
         <iframe
-        title="Homepage Video" 
-        width="560" 
-        height="315" 
-        src="https://www.youtube.com/embed/fLUYEp_cHmw" 
-        frameborder="0" 
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+          title="Homepage Video"
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/fLUYEp_cHmw"
+          frameborder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
         </iframe>
         <p>
           <code>Welcome to the Antisocial Smoke Society</code>
         </p>
         <p>
-        
+
         </p>
         <script data-ad-client="ca-pub-6202902142885850" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-      </header> 
-        )
-    }
+      </header>
+    )
+  }
 }
 
 export default Homepage;
